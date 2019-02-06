51a48666ee175a93b27f130d4eab03b548975b11
DarrenZLew <darrenzlew@gmail.com>
2019-02-05 22:39:42 -0600

    Update voting to only show thumbs up. Only show if there's an upvote.

--- src/components/UI/BoardComponents/Voting/index.js
+++ src/components/UI/BoardComponents/Voting/index.js
@@ -3,20 +3,19 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./index.scss";
 
 const Voting = props => {
-  const { upvotes = 0, downvotes = 0 } = props;
+  const { upvotes = 0 } = props;
   return (
     <div id="vote-container">
-      <div className="vote-thumbs-container">
-        <div className="vote-thumb">
+      <div className={`vote-thumbs-container ${upvotes ? "has-votes" : ""}`}>
+        <div
+          onClick={e => {
+            e.stopPropagation();
+          }}
+          className={`vote-thumb  ${upvotes ? "has-votes" : ""}`}
+        >
           <FontAwesomeIcon icon="thumbs-up" size="sm" color={"#edcd89"} />
         </div>
-        <div className="vote-counter">{upvotes}</div>
-      </div>
-      <div className="vote-thumbs-container">
-        <div className="vote-thumb">
-          <FontAwesomeIcon icon="thumbs-down" size="sm" color={"#edcd89"} />
-        </div>
-        <div className="vote-counter">{downvotes}</div>
+        {upvotes !== 0 && <div className="vote-counter">{upvotes}</div>}
       </div>
     </div>
   );

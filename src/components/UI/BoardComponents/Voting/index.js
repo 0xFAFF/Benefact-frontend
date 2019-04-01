90e2f5c1dcae1e3764e3d52c374d543f7715c1aa
DarrenZLew <darrenzlew@gmail.com>
2019-03-31 23:45:10 -0500

    Add in current user votes.

--- src/components/UI/BoardComponents/Voting/index.js
+++ src/components/UI/BoardComponents/Voting/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { AuthConsumer } from "../../../Auth/AuthContext";
+import { parseToken } from "../../../../utils";
 import "./index.scss";
 
 const Voting = props => {
@@ -7,60 +9,71 @@ const Voting = props => {
     votes = [],
     size = "sm",
     defaultDisplay = false,
-    counterWidth = "50px",
+    // counterWidth = "50px",
     onUpdateVote
   } = props;
   const totalVotes = votes.reduce((arr, curr) => {
     const { count = 0 } = curr;
     return arr + count;
   }, 0);
-
   return (
-    <div id="vote-container" className="flex-row">
-      <div
-        className={`flex-row vote-thumbs-container ${
-          totalVotes ? "has-votes" : ""
-        }`}
-      >
-        <div
-          onClick={e => {
-            e.stopPropagation();
-            onUpdateVote("add");
-          }}
-          className={`vote-counter  ${
-            totalVotes || defaultDisplay ? "has-votes" : ""
-          }`}
-        >
-          <FontAwesomeIcon
-            icon="arrow-circle-up"
-            size={size}
-            color={"#13b405"}
-          />
-        </div>
-        {(totalVotes !== 0 || defaultDisplay) && (
-          <div
-            onClick={e => {
-              e.stopPropagation();
-              onUpdateVote("subtract");
-            }}
-            className={`vote-counter  ${
-              totalVotes || defaultDisplay ? "has-votes" : ""
-            }`}
-          >
-            <FontAwesomeIcon
-              icon="arrow-circle-down"
-              size={size}
-              color={"#fd0a0a"}
-            />
+    <AuthConsumer>
+      {token => {
+        const { id } = parseToken(token);
+        const currUserVotes = votes.reduce((arr, curr) => {
+          const { count = 0, userId } = curr;
+          if (userId === id) return arr + count;
+          return arr;
+        }, 0);
+        return (
+          <div id="vote-container" className="flex-row">
+            <div
+              className={`flex-row vote-thumbs-container ${
+                totalVotes ? "has-votes" : ""
+              }`}
+            >
+              <div
+                onClick={e => {
+                  e.stopPropagation();
+                  onUpdateVote("add");
+                }}
+                className={`vote-counter  ${
+                  totalVotes || defaultDisplay ? "has-votes" : ""
+                }`}
+              >
+                <FontAwesomeIcon
+                  icon="arrow-circle-up"
+                  size={size}
+                  color={"#13b405"}
+                />
+              </div>
+              {(totalVotes !== 0 || defaultDisplay) && (
+                <div
+                  onClick={e => {
+                    e.stopPropagation();
+                    onUpdateVote("subtract");
+                  }}
+                  className={`vote-counter  ${
+                    totalVotes || defaultDisplay ? "has-votes" : ""
+                  }`}
+                >
+                  <FontAwesomeIcon
+                    icon="arrow-circle-down"
+                    size={size}
+                    color={"#fd0a0a"}
+                  />
+                </div>
+              )}
+              {(defaultDisplay || (totalVotes !== 0 && !defaultDisplay)) && (
+                <div className="vote-tracker">
+                  {totalVotes} / {currUserVotes}
+                </div>
+              )}
+            </div>
           </div>
-        )}
-        {(defaultDisplay || (totalVotes !== 0 && !defaultDisplay)) && (
-          <div className="vote-tracker" style={{ width: counterWidth }}>
-            {totalVotes}
-          </div>
-        )}
-      </div>
-    </div>
+        );
+      }}
+    </AuthConsumer>
   );
 };
 

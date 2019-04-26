6c5a3fd9a22c501ebca040951374bfc6b7aa95a9
DarrenZLew <darrenzlew@gmail.com>
2019-04-26 16:20:37 -0500

    Update light theme in THEMES file.

--- src/constants/THEMES.js
+++ src/constants/THEMES.js
@@ -2,7 +2,21 @@ const THEMES = () => {
   return {
     currentTheme: "dark",
     themes: {
-      light: {},
+      light: {
+        "bg-primary": "#eef1f5",
+        "bg-navbar": "var(--bg-primary)",
+        "navbar-text-color": "var(--text-color)",
+        "modal-bg-color": "#ffffff",
+        "bg-shadow": "0px 0px 13px 0px rgba(98, 98, 102, 0.16)",
+        "card-border": "lightgrey",
+        "card-primary": "#ffffff",
+        "card-hover": "#e5eae9",
+        "column-hover": "#f3f3f3",
+        "text-color": "black",
+        "text-bg-color": "#f0f0f0",
+        "icon-color": "black",
+        "modal-icon-color": "rgb(26, 25, 25)"
+      },
       dark: {
         "bg-primary": "#333",
         "card-primary": "#444",

d9b95a5b1e424313fabd4a91e825372a6f31ecb0
Darren Lew <darrenzlew@gmail.com>
2019-05-23 08:01:40 -0700

    Update formatting files

--- src/utils/formatTime.js
+++
@@ -1,5 +0,0 @@
-import moment from "moment";
-
-export const formatTime = (time, format = "MMM D [at] h:mm A z") => {
-  return moment.unix(time).format(format);
-};
--- src/utils/formatting.js
+++ src/utils/formatting.js
@@ -1,3 +1,5 @@
 import moment from "moment";
+
 const formatTime = time => moment.unix(time).format("MMM D [at] h:mm A z");
+
 export { formatTime };

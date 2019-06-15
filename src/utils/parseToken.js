ca2ac65454d5f01c0bd70988dfcc6f76066f9d76
Alex Sherman <asherman1024@gmail.com>
2019-06-14 21:34:17 -0500

    Combine some util scripts

--- src/utils/parseToken.js
+++
@@ -1,8 +0,0 @@
-const parseToken = (token = "") => {
-  const base64parts = token.split(".");
-  if (Array.isArray(base64parts) && base64parts.length === 3)
-    return JSON.parse(window.atob(base64parts[1].replace(/-/g, "+").replace(/_/g, "/")));
-  return "";
-};
-
-export default parseToken;
